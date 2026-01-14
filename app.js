cae289b067fd3942580e9302ad1d861c9c704736
Nicolas Mendoza <nicolasmenndoza@gmail.com>
2026-01-14 11:36:57 -0300

    configuracion terminada del servidor

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 //importar express
 const express = require("express");
 
+const path = require("path");
+
 //se inicializa el servidor
 const app = express();
 
 //middleware configurado antes de las rutas
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
+// configurar EJS
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
 
 // importar rutas
-const topicsRoutes = require("./routes/topics.routes");
+app.use("/", require("./routes/home.routes"));
+app.use("/topics", require("./routes/topics.routes"));
+app.use("/links", require("./routes/links.routes"));
 
-// usar rutas
-app.use("/", topicsRoutes);
 
 //definir el puerto
 const PORT = 5555;
 
 //levantar el servidor
 app.listen(PORT, () => {
-    console.log(`servidor escuchando en http://localhost:${PORT}`);
+    console.log(`servidor en http://localhost:${PORT}`);
 });
